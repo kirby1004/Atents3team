c4732bfdb0b78d6e0004fdca01404ce9a07e092b
kirby1004 <carby1004@gmail.com>
2023-04-30 12:02:50 +0900

    20230430_GY_Inventory&EquipmentSystem BugFix

--- RPG_Forest/Assets/02.Scripts/UI/Inventory/InventoryItem.cs
+++ RPG_Forest/Assets/02.Scripts/UI/Inventory/InventoryItem.cs
@@ -53,7 +53,7 @@ public class InventoryItem : MonoBehaviour , IBeginDragHandler, IEndDragHandler 
     }
 
 
-    // 입력받은 트랜스폼과 부모 교환 true 일때는 자리까지 바꿈
+    // 입력받은 트랜스폼과 부모 교환 false 일때는 참조대상만 , true 일때는 부모변경후 포지션이동까지
     public void ChangeParent(Transform p, bool update = false)
     {
         parentAfterDrag = p;
--- RPG_Forest/Assets/02.Scripts/UI/Inventory/InventorySlot.cs
+++ RPG_Forest/Assets/02.Scripts/UI/Inventory/InventorySlot.cs
@@ -10,7 +10,7 @@ public class InventorySlot : MonoBehaviour , IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         // 슬롯에 아이템이 없을때는 그 슬롯으로 이동
-        if (transform.childCount == 0)
+        if (transform.GetComponentInChildren<InventoryItem>() == null)
         {
 
             InventoryItem inventorySlotItem = eventData.pointerDrag.GetComponent<InventoryItem>();
@@ -23,23 +23,47 @@ public class InventorySlot : MonoBehaviour , IDropHandler
         //슬롯에 아이템이 있을때는 서로의 슬롯을 바꿔줌 착용중인 장비가 들어오면 장착시키기
         //수정예정 1 타입고려안하고 장착시켜서 무기칸에 투구가 들어가지기도 함
         //타입이 같을경우에만 이동시키게 할 예정
-        else if (transform.childCount == 1)
+        //수정완료
+        else if (transform.GetComponentInChildren<InventoryItem>() != null)
         {
             InventoryItem mySlotItem = transform.GetComponentInChildren<InventoryItem>();
-            InventoryItem inventorySlotItem = eventData.pointerDrag.GetComponentInChildren<InventoryItem>();
-            if(mySlotItem != null)
+            InventoryItem dropItem = eventData.pointerDrag.GetComponentInChildren<InventoryItem>();
+            int SlotNum = Gamemanager.instance.myUIManager.inventoryManager.FindEmptySlot();
+            if(dropItem.GetComponent<EquipmentItem>() != null)
             {
-                mySlotItem.ChangeParent(inventorySlotItem.parentAfterDrag,true);
-                if (inventorySlotItem.GetComponent<EquipmentItem>() != null)
+                if( dropItem.item.MyEquipmentType == mySlotItem.item.MyEquipmentType)
                 {
-                    mySlotItem.AddComponent<EquipmentItem>();
+                    transform.GetComponentInChildren<InventoryItem>().ChangeParent(dropItem.parentAfterDrag, true);
+                    Destroy(dropItem.GetComponent<EquipmentItem>());
+                    dropItem.ChangeParent(transform);
+                    transform.GetComponentInChildren<InventoryItem>().AddComponent<EquipmentItem>();
+                }
+                else
+                {
+                    if(SlotNum == -1)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Gamemanager.instance.myUIManager.inventoryManager.InsertItem(dropItem, SlotNum);
+                        Destroy(dropItem.GetComponent<EquipmentItem>());
+                    }
                 }
             }
-            inventorySlotItem.ChangeParent(transform);
-            if (inventorySlotItem.GetComponent<EquipmentItem>() != null)
+            else if (dropItem.GetComponent<EquipmentItem>() == null)
             {
-                Destroy(inventorySlotItem.GetComponent<EquipmentItem>());
+                if (SlotNum == -1)
+                {
+                    return;
+                }
+                else
+                {
+                    Gamemanager.instance.myUIManager.inventoryManager.InsertItem(dropItem, SlotNum);
+                }
+                dropItem.ChangeParent(transform);
             }
+            
         }
     }
 
