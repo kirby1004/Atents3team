19ce1118438dc242a78537f158351d95f81f091a
DESKTOP-PKNQEOK\byeon <tnals586@naver.com>
2023-05-12 14:54:16 +0900

    20230512

--- RPG_Forest/Assets/02.Scripts/Characters/Skill/Skill.cs
+++ RPG_Forest/Assets/02.Scripts/Characters/Skill/Skill.cs
@@ -2,32 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface ISkill
+{
+    void Use()
+    {
+
+    }
+}
+
 public abstract class Skill : MonoBehaviour
 {
     [SerializeField]
-    protected SkillData _skillData;
-    public SkillData skillData { get { return _skillData; }  set { _skillData = value;  } }
+    protected SkillData skillData;
 
-    protected float AttackDamage;
-    protected float ccTime;
-    protected float BuffStatus;
-    protected float BuffTime;
-
-    protected void SetValue(SkillType type)
-    {
-        switch (type)
-        {
-            case SkillType.Attack:
-                AttackDamage = skillData.SkillValue1;
-                ccTime = skillData.SkillValue2;
-                break;
-            case SkillType.Buff:
-                BuffStatus = skillData.SkillValue1;
-                BuffTime = skillData.SkillValue2;
-                break;
-
-        }
-    }
-
-    public abstract void Use();
+    [SerializeField]
+    protected Transform HitPoint;
 }
--- RPG_Forest/Assets/02.Scripts/Characters/Skill/SkillData.cs
+++ RPG_Forest/Assets/02.Scripts/Characters/Skill/SkillData.cs
@@ -6,47 +6,36 @@ using UnityEngine;
 public class SkillData : ScriptableObject
 {
     [SerializeField]
-    protected Sprite _skillImage;
-    public Sprite SkillImage { get { return _skillImage; } }
+    protected Sprite _image;
+    public Sprite Image { get { return _image; } }
     [SerializeField]
-    protected string _skillName;
-    public string SkillName { get { return _skillName; } }
+    protected string _name;
+    public string Name { get { return _name; } }
     [SerializeField]
-    protected float _skillValue1; //Attack인지 Buff 인지에 따라 Attack-> Damage, Buff-> Buff 수치
-    public float SkillValue1 { get { return _skillValue1; } }
+    protected float _value1; //Attack인지 Buff 인지에 따라 Attack-> Damage, Buff-> Buff 수치
+    public float Value1 { get { return _value1; } }
     [SerializeField]
-    protected float _skillValue2; //Attack인지 Buff 인지에 따라 Attack-> CC기 시간, Buff-> Buff 유지 시간
-    public float SkillValue2 { get { return _skillValue2; } }
+    protected float _value2; //Attack인지 Buff 인지에 따라 Attack-> CC기 시간, Buff-> Buff 유지 시간
+    public float Value2 { get { return _value2; } }
     [SerializeField]
-    protected float _skillRange;
-    public float SkillRange { get { return _skillRange; } }
+    protected float _range;
+    public float Range { get { return _range; } }
     [SerializeField]
-    protected float _skillDistance;
-    public float SkillDistance { get { return _skillDistance; } }
+    protected float _distance;
+    public float Distance { get { return _distance; } }
     [SerializeField]
     protected float _coolTime;
     public float CoolTime { get { return _coolTime; } }
     [SerializeField]
-    protected float _skillSpeed;
-    public float SkillSpeed { get { return _skillSpeed; } }
+    protected float _Speed;
+    public float Speed { get { return _Speed; } }
     [SerializeField]
-    protected CrowndControlType _crowdControlType;
-    public CrowndControlType crowdControlType { get { return _crowdControlType; } }
-    [SerializeField]
-    protected SkillType _skillType;
-    public SkillType skillType { get { return _skillType; } }
-    [SerializeField]
-    protected GameObject _skillEffect;
-    public GameObject SkillEffect { get { return _skillEffect; } set { _skillEffect = value; } }
-
+    protected CROWNDCONTROLTYPE _crowdControlType;
+    public CROWNDCONTROLTYPE CrowdControlType { get { return _crowdControlType; } }
 }
 
-public enum SkillType
-{
-    Attack, Buff
-}
 
-public enum CrowndControlType
+public enum CROWNDCONTROLTYPE
 {
     None,Burn, Freeze, Electricshock
 }
