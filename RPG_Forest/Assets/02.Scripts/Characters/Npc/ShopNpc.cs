1204878dbbb69f0368f2eb0161d99f3839a5047c
DESKTOP-PKNQEOK\byeon <tnals586@naver.com>
2023-05-16 15:11:33 +0900

    20230516_SM_SkillManager,NPC,SpringArmUpdate

--- RPG_Forest/Assets/02.Scripts/Characters/Npc/ShopNpc.cs
+++ RPG_Forest/Assets/02.Scripts/Characters/Npc/ShopNpc.cs
@@ -13,17 +13,27 @@ public class ShopNpc : NpcProperty
     }
     public void ShopView(GameObject obj)
     {
-        obj.GetComponent<PlayerController>().SpringArm.gameObject.GetComponent<SpringArm>().ChangeViewPoint(ViewPoint);
-        obj.transform.position = PlayerPoint.position;
-        obj.transform.rotation = Quaternion.Euler(0, PlayerPoint.rotation.eulerAngles.y, 0);
-        
+        StartCoroutine(ShopViewing(obj));
     }
+
+    IEnumerator ShopViewing(GameObject obj)
+    {
+        obj.transform.position = PlayerPoint.position;
+        obj.transform.localRotation = playerPoint.localRotation;
+        //while (!Mathf.Approximately(obj.GetComponent<PlayerController>().SpringArm.GetComponent<SpringArm>().CurRot.y, PlayerPoint.rotation.eulerAngles.y))
+        {
+            //obj.GetComponent<PlayerController>().SpringArm.GetComponent<SpringArm>().CurRot=Vector3.Lerp(obj.GetComponent<PlayerController>().SpringArm.GetComponent<SpringArm>().CurRot, PlayerPoint.rotation.eulerAngles, Time.deltaTime * 3.0f);
+            yield return null;
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<IinterPlay>().SetisObjectNear(true);
         //other.gameObject.GetComponent<IinterPlay>().OpenUi.AddListener(() => { ShopManager.Inst.OpenShop(_npctype); });
         //other.gameObject.GetComponent<IinterPlay>().CloseUi.AddListener();
-        other.gameObject.GetComponent<IinterPlay>()?.interPlay.AddListener(()=> { ShopView(other.gameObject); });
+        other.gameObject.GetComponent<IinterPlay>()?.OpenUi.AddListener(()=> { ShopView(other.gameObject); });
 
     }
 
