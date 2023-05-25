4406712a89a1aa6816668fc53971a4f52d2dee4b
DESKTOP-PKNQEOK\byeon <tnals586@naver.com>
2023-05-25 12:39:29 +0900

    20230525_SM_VillageScene

--- RPG_Forest/Assets/02.Scripts/Characters/Player/PlayerController.cs
+++ RPG_Forest/Assets/02.Scripts/Characters/Player/PlayerController.cs
@@ -110,8 +110,7 @@ public class PlayerController : CharacterMovement_V2, IBattle,IinterPlay
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (isEnterUI == true) return;
-
+                if (isEnterUI) return;
                 myAnim.SetTrigger("Attack"); // 공격 애니메이션 실행
             }
 
@@ -229,6 +228,11 @@ public class PlayerController : CharacterMovement_V2, IBattle,IinterPlay
         clickCount = 0;
         while (true)
         {
+            if (isEnterUI)
+            {
+                clickCount = 0;
+                AttackExit();
+            }
             if (Input.GetMouseButtonDown(0))
             {
                 clickCount++;
