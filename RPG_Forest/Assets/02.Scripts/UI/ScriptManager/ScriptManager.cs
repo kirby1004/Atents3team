1b38609deea0de98a94f11afa3adae149a39ba0b
kirby1004 <carby1004@gmail.com>
2023-05-27 15:10:38 +0900

    20230527_GY_TextScript Load Alpha

--- RPG_Forest/Assets/02.Scripts/UI/ScriptManager/ScriptManager.cs
+++ RPG_Forest/Assets/02.Scripts/UI/ScriptManager/ScriptManager.cs
@@ -3,33 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MySctipts
 {
     public List<TextScript> TextScript;
 }
 
-[Serializable]
+[System.Serializable]
 public class TextScript
 {
     public int id;
     public string Text;
 }
 
-public class ScriptManager : MonoBehaviour
+public class ScriptManager : Singleton<ScriptManager>
 {
-    public ScriptWindow myWindow;    
+    public ScriptWindow myWindow;
 
+    //public MySctipts mySctipts = new();
+
+    public int index = 0;
+
+    public MySctipts myScripts;
+    public bool isScriptEnd = false;
+
+    private void Awake()
+    {
+        base.Initialize();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        TextAsset textAsset = Resources.Load<TextAsset>("JSon/Test1");
 
+        myScripts = JsonUtility.FromJson<MySctipts>(textAsset.ToString());
+        //foreach(TextScript script in mySctipts.TextScript)
+        //{
+        //    Debug.Log($"{script.id},{script.Text}");
+        //}
+        for(int i = 0; i< myScripts.TextScript.Count; i++)
+        {
+            Debug.Log($"{myScripts.TextScript[i].id} , {myScripts.TextScript[i].Text}");
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isScriptEnd)
+            {
+                myWindow.ScriptAdd((ScriptWindow.IdType)myScripts.TextScript[index].id, myScripts.TextScript[index].Text);
+                index++;
+            }
+        }
+    }
+
+    public void RefreshScript(string text)
+    {
+        TextAsset textAsset = Resources.Load<TextAsset>($"JSon/{text}");
+
+        myScripts = JsonUtility.FromJson<MySctipts>(textAsset.ToString());
+        isScriptEnd = false;
+        index = -1;
     }
 
 }
--- RPG_Forest/Assets/02.Scripts/UI/ScriptManager/ScriptWindow.cs
+++ RPG_Forest/Assets/02.Scripts/UI/ScriptManager/ScriptWindow.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ScriptWindow : MonoBehaviour
 {
+    public float ShowDelay = 0.05f;
+    public enum IdType
+    {
+        normal,Start,End,NewLoad
+    }
+    public UnityAction DoneAction;
+
+    public TMP_Text myText;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -15,4 +26,44 @@ public class ScriptWindow : MonoBehaviour
     {
         
     }
+
+    public void ScriptAdd(IdType id , string text)
+    {
+        switch (id)
+        {
+            case IdType.normal:
+                StartCoroutine(ShowScript(text));
+                break; 
+            case IdType.Start:
+                DoneAction = null;
+                break;
+            case IdType.End:
+                StartCoroutine(ShowScript(text));
+                DoneAction += () => ScriptManager.Inst.isScriptEnd = true;
+                break; 
+            case IdType.NewLoad:
+                ScriptManager.Inst.RefreshScript(text);
+                break;
+        }
+
+        
+
+    }
+
+    IEnumerator ShowScript(string text)
+    {
+        StringBuilder sb = new StringBuilder();
+        WaitForSeconds ws = new WaitForSeconds(ShowDelay);
+        sb.Clear();
+        int index = 0;
+        while (sb.Length<text.Length)
+        {
+            sb.Append(text[index]);
+            myText.text = sb.ToString();
+            index++;
+            yield return ws;
+        }
+        DoneAction?.Invoke();
+    }
+
 }
